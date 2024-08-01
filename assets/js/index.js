f3cafbe5343f3111e0e713cf8db7ae2e7e0810d1
David Tanasescu <davidtanasescu09@gmail.com>
2024-08-01 18:31:27 +0200

    fixed u shit

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,23 +7,27 @@ document.addEventListener("click", function () {
     overlay.classList.add("fade-out");
     content.style.display = "flex";
 
-    gsap.to(".semi-transparent-container", { opacity: 1, duration: 1 });
+    if (typeof gsap !== 'undefined') {
+        gsap.to(".semi-transparent-container", { opacity: 1, duration: 1 });
+    }
 });
 
 function animateTitle() {
-    gsap.to("#dynamic-title", {
-        x: "-100%",
-        duration: 15,
-        repeat: -1,
-        ease: "none",
-    });
+    if (typeof gsap !== 'undefined') {
+        gsap.to("#dynamic-title", {
+            x: "-100%",
+            duration: 15,
+            repeat: -1,
+            ease: "none",
+        });
+    }
 }
 
 animateTitle();
 
-function animateName() {
-    const element = document.getElementById("moving-name");
-    const fullText = "damon";
+function animateBio() {
+    const element = document.getElementById("moving-bio");
+    const fullText = "ion fw fake niggas";
     const interval = 300;
 
     let index = 0;
@@ -31,15 +35,15 @@ function animateName() {
 
     setInterval(() => {
         if (adding) {
-            element.textContent = "@" + fullText.slice(0, index + 1);
+            element.textContent = fullText.slice(0, index + 1);
             index++;
             if (index === fullText.length) adding = false;
         } else {
-            element.textContent = "@" + fullText.slice(0, index);
+            element.textContent = fullText.slice(0, index);
             index--;
             if (index === 0) adding = true;
         }
     }, interval);
 }
 
-animateName();
+animateBio();
